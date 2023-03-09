d7e11a2f6080346cdfefe8147b6b89a7c14aae03
zchips <zchips@gmail.com>
2023-03-09 15:11:42 -0700

    wip 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './less/index.less'
 
 // Your code goes here!
+
+
+window.onload = function (evt) {
+    console.log(`event ${evt.type}fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'Ready to go!'
+
+    //copy text and replace the existing text on the display
+    window.addEventListener('copy', () =>{
+        navigator.clipboard.readText().then (text => {heading.textContent += text})
+    })
+
+    //  upon the click action, the element clicked will mirror the original by 180degs
+    document.body.addEventListener('click', evt =>{
+        evt.target.classList.toggle('mirror')
+    })
+
+    // doubleclicking takes the element and replaces it with a empty string
+    document.body.addEventListener('dblclick', evt => {evt.target.outerHTML = ''})
+
+
+    // pressing of a key can execute a override on the page
+    window.addEventListener('keydown', evt =>{
+        if (evt.key == 6){
+            document.body.innerHTML = '<h1> lets shut it down</h1>'
+        }
+    })
+
+    // collecting and doing something with mouse-movement.  
+    document.body.addEventListener('mousemove', evt => {
+        const {clientX, clientY} = evt
+
+    })
+    
+
+}
